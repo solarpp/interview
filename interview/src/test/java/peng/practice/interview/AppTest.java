e6b3139864d866181dcf8d46750996b4ecf2e281
solarpp <sunpp_msn@hotmail.com>
2014-12-08 17:02:17 +0800

    create an interview maven project with testng

---
+++ interview/src/test/java/peng/practice/interview/AppTest.java
@@ -0,0 +1,17 @@
+package peng.practice.interview;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+@Test
+public class AppTest 
+{
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
